b5cb11b476204861cdb1ba667e51a3de348c791b
Dama <dam4rusxp@googlemail.com>
2021-09-03 22:31:05 +0200

    Improve error handling

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import logging
 import os
-import sys
-import traceback
 
 from discord.ext import commands
+from discord.ext.commands import CommandError, Context
 from discord_slash import SlashCommand
 from dotenv import load_dotenv
 
@@ -19,10 +18,12 @@ class DamaBot(commands.Bot):
         self.load_extension("cogs.music")
         log.info(f"Online as {self.user.name}. ID: {self.user.id}")
 
-    async def on_command_error(self, ctx, error):
-        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+    async def on_command_error(self, ctx: Context, error: CommandError):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("Missing argument '" + error.param.name + "'")
+            return
+
+        log.error(f"Error during command: {ctx.message.clean_content}", exc_info=error)
 
 
 def main():
