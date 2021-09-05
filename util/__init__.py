f5300f209083dd918429985fe93d9744ac5adb5a
Dama <dam4rusxp@googlemail.com>
2021-09-05 22:24:06 +0200

    Log command and exception

--- util/__init__.py
+++ util/__init__.py
@@ -3,9 +3,23 @@ import logging
 from functools import wraps
 
 from discord import NotFound
+from discord.ext.commands import Context
 from discord_slash import SlashContext
 
 _log = logging.getLogger(__name__)
+
+
+def get_command(ctx) -> str:
+    """
+    Retrieve the original command as a string from a commands.Context or SlashContext.
+    """
+    if isinstance(ctx, Context):
+        return ctx.message.clean_content
+    elif isinstance(ctx, SlashContext):
+        result = "/" + ctx.command
+        result += " " + ctx.subcommand_name if ctx.subcommand_name else ""
+        result += " " + " ".join([f"{k}:{v}" for (k, v) in ctx.kwargs.items()])
+        return result
 
 
 def auto_defer(func):
