3e1704b46e55ae9f9b7474daba0665bf1c590a44
Dama <dam4rusxp@googlemail.com>
2022-02-04 21:27:17 +0100

    Support .yoink with emoji-links

--- cogs/emoji/__init__.py
+++ cogs/emoji/__init__.py
@@ -24,19 +24,28 @@ class Emoji(Cog):
     @commands.command()
     async def yoink(self, ctx: Context, emoji: str, name=""):
         match = re.match("^<a?:(.+):(\\d+)>$", emoji)
-        if not match:
-            await ctx.reply("Please supply an emoji.")
-            return
 
-        name = match.group(1) if not name else name
-        emoji_id = match.group(2)
+        if match:
+            name = match.group(1) if not name else name
+            emoji_id = match.group(2)
+        else:
+            match = re.match("^https://cdn\\.discordapp\\.com/emojis/(\\d+)\\..*$", emoji)
+
+            if not match:
+                await ctx.reply("Please supply an emoji or a link to one.")
+                return
+            elif match and not name:
+                await ctx.reply("Please supply a name with an emoji-link.")
+                return
+
+            emoji_id = match.group(1)
 
         async with aiohttp.ClientSession() as session:
             async with session.get(f"https://cdn.discordapp.com/emojis/{emoji_id}") as resp:
                 data = await resp.read()
 
         if not data:
-            await ctx.reply("Could not fetch the emoji image")
+            await ctx.reply("Could not fetch the emoji image.")
             return
 
         emoji = await ctx.guild.create_custom_emoji(name=name, image=data)
