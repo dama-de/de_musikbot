8ffdcf192cfae4b4b6629aefbd0416c30ad27261
Dama <dam4rusxp@googlemail.com>
2022-12-02 00:39:07 +0100

    Reorder functions/methods

--- main.py
+++ main.py
@@ -11,17 +11,27 @@ from util.config import Config
 log = logging.getLogger(__name__)
 
 
+def main():
+    setup_logging()
+
+    load_dotenv(verbose=True)
+
+    bot = DamaBot()
+    bot.run(os.environ["DISCORD_TOKEN"])
+
+
+def setup_logging():
+    _loglevel = os.environ["LOG_LEVEL"] if "LOG_LEVEL" in os.environ else "INFO"
+    logging.basicConfig(level=_loglevel, format="%(levelname)-7s | %(asctime)s | %(name)-18s | %(message)s")
+    logging.getLogger("discord").setLevel("INFO")
+
+
 class DamaBot(commands.Bot):
 
     def __init__(self, **kwargs):
         # Init client with all intents enabled
         kwargs["intents"] = Intents.all()
         super().__init__(command_prefix=os.environ["PREFIX"], **kwargs)
-
-    async def load_ext(self, cog: str):
-        if cog not in self.extensions:
-            log.info(f"Loading {cog}")
-            await self.load_extension(cog)
 
     async def setup_hook(self):
         config = Config("admin")
@@ -32,6 +42,11 @@ class DamaBot(commands.Bot):
         if "cogs.enabled" in config.data:
             for cog in config.data["cogs.enabled"]:
                 await self.load_ext(cog)
+
+    async def load_ext(self, cog: str):
+        if cog not in self.extensions:
+            log.info(f"Loading {cog}")
+            await self.load_extension(cog)
 
     async def on_ready(self):
         log.info(f"Online as {self.user.name}. ID: {self.user.id}")
@@ -45,21 +60,6 @@ class DamaBot(commands.Bot):
             log.warning("Passing CommandError", error)
         else:
             log.error(f"Error during command: {ctx.message.clean_content}", exc_info=error)
-
-
-def main():
-    load_dotenv(verbose=True)
-
-    setup_logging()
-
-    bot = DamaBot()
-    bot.run(os.environ["DISCORD_TOKEN"])
-
-
-def setup_logging():
-    _loglevel = os.environ["LOG_LEVEL"] if "LOG_LEVEL" in os.environ else "INFO"
-    logging.basicConfig(level=_loglevel, format="%(levelname)-7s | %(asctime)s | %(name)-18s | %(message)s")
-    logging.getLogger("discord").setLevel("INFO")
 
 
 if __name__ == "__main__":
