bea69e1cb585cf4572a48e04de8ad46c05026747
Dama <dam4rusxp@googlemail.com>
2022-12-04 15:27:51 +0100

    Wip

--- cogs/admin/__init__.py
+++ cogs/admin/__init__.py
@@ -32,7 +32,7 @@ class Admin(Cog):
     async def cog_unload(self) -> None:
         self._save()
 
-    async def cog_check(self, ctx):
+    async def cog_check(self, ctx: Context):
         return await ctx.bot.is_owner(ctx.author)
 
     @command(hidden=True)
@@ -91,7 +91,7 @@ class Admin(Cog):
         else:
             await ctx.message.add_reaction("\N{BLACK QUESTION MARK ORNAMENT}")
 
-    async def _react_ok(self, ctx):
+    async def _react_ok(self, ctx: Context):
         await ctx.message.add_reaction("\N{WHITE HEAVY CHECK MARK}")
 
     @command(hidden=True)
@@ -100,7 +100,7 @@ class Admin(Cog):
         await self._react_ok(ctx)
 
     @command(hidden=True)
-    async def leave(self, ctx: Context, server_id=None):
+    async def leave(self, ctx: Context, server_id: int = None):
         if not server_id:
             server_id = ctx.guild.id
         guild = ctx.bot.get_guild(server_id)
--- util/__init__.py
+++ util/__init__.py
@@ -1,22 +1,26 @@
 import logging
 import os
 
+from discord.app_commands import Command
 from discord.ext.commands import Context
-
-if "SKIP_SLASH" not in os.environ:
-    from discord_slash import SlashContext
 
 _log = logging.getLogger(__name__)
 
 
-def get_command(ctx) -> str:
+# TODO Printing a slash command
+# elif isinstance(ctx, SlashContext):
+#     result = "/" + ctx.command
+#     result += " " + ctx.subcommand_name if ctx.subcommand_name else ""
+#     result += " " + " ".join([f"{k}:{v}" for (k, v) in ctx.kwargs.items()])
+#     return result
+
+
+def get_command(ctx: Context) -> str:
     """
     Retrieve the original command as a string from a commands.Context or SlashContext.
     """
-    if isinstance(ctx, Context):
-        return ctx.message.clean_content
-    elif isinstance(ctx, SlashContext):
-        result = "/" + ctx.command
-        result += " " + ctx.subcommand_name if ctx.subcommand_name else ""
-        result += " " + " ".join([f"{k}:{v}" for (k, v) in ctx.kwargs.items()])
-        return result
+    if ctx.interaction:
+        maybe_command = ctx.interaction.command()
+        if isinstance(maybe_command, Command):
+            return "/" + maybe_command.qualified_name
+    return ctx.message.clean_content()
