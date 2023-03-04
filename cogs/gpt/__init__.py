5a73986ae266701b1167fc0cebf27e3669cde77c
Dama <dam4rusxp@googlemail.com>
2023-03-04 15:22:01 +0100

    Remove old GPT commands

--- cogs/gpt/__init__.py
+++ cogs/gpt/__init__.py
@@ -1,10 +1,8 @@
 import logging
-import re
 from typing import List
 
-import discord
-from discord import Interaction, Message
-from discord.ext.commands import Cog, Bot, Context, hybrid_command, is_owner
+from discord import Message
+from discord.ext.commands import Cog, Bot, Context, hybrid_command
 
 import util
 from cogs.gpt.ai import AIError
@@ -21,43 +19,6 @@ class GPT(Cog):
     def __init__(self, bot: Bot) -> None:
         self._config = GPTConfig()
         self._bot = bot
-
-    @hybrid_command(hidden=True, enabled=False)
-    @is_owner()
-    async def gpt_model(self, ctx: Context):
-        """Choose the GPT models used"""
-        await ctx.reply(content="Choose the models", view=SettingsView(self._config), ephemeral=True)
-
-    @hybrid_command(enabled=False)
-    async def gpt(self, ctx: Context, *, prompt: str):
-        """Let a GPT-3 AI respond to your prompt. Try \"Tell me a joke!\""""
-        async with ctx.typing():
-            try:
-                text = await ai.completion(prompt, model=self._config.model, temperature=self._config.temperature,
-                                           max_tokens=self._config.max_tokens,
-                                           presence_penalty=self._config.presence_penalty,
-                                           user=ctx.author.name)
-                text = f"> {prompt}{text}"
-                await util.split_message(text, ctx)
-            except AIError as e:
-                await ctx.reply(str(e))
-
-    @hybrid_command(enabled=False)
-    async def code(self, ctx: Context, *, prompt: str):
-        """Let a GPT-3 AI complete your code. Format your prompt like a comment, and mention the language."""
-        async with ctx.typing():
-            try:
-                text = await ai.completion(prompt, model=self._config.code_model,
-                                           temperature=self._config.code_temperature,
-                                           max_tokens=self._config.max_tokens,
-                                           presence_penalty=self._config.presence_penalty,
-                                           echo=True,
-                                           user=ctx.author.name)
-                # Remove the line-prefix
-                text = re.sub(r"^\+", "", text, flags=re.MULTILINE)
-                await util.split_message(text, ctx, prefix="```\n", suffix="\n```")
-            except AIError as e:
-                await ctx.reply(str(e))
 
     @hybrid_command()
     async def chatgpt(self, ctx: Context, *, prompt: str):
@@ -131,32 +92,3 @@ class GPT(Cog):
             history.insert(0, message.reference.resolved)
             message = message.reference.resolved
         return history
-
-
-class SettingsView(discord.ui.View):
-    def __init__(self, config: GPTConfig):
-        super().__init__()
-        self.add_item(ModelDropdown(config, "model"))
-        self.add_item(ModelDropdown(config, "code_model"))
-
-
-class ModelDropdown(discord.ui.Select):
-    def __init__(self, config: GPTConfig, key: str):
-        self._config = config
-        self._key = key
-
-        models = ["text-davinci-003", "text-curie-001", "code-davinci-002", "code-cushman-001"]
-        selected_model = self._config.data[key]
-
-        options = [
-            discord.SelectOption(label=f"{key}: {model}", value=model, default=(model == selected_model))
-            for model in models
-        ]
-
-        super().__init__(placeholder=f"Choose a {key}", options=options)
-
-    async def callback(self, interaction: Interaction):
-        self._config.data[self._key] = self.values[0]
-        self._config.save()
-        # Edit the original interaction message and recreate the view to reflect saved settings
-        await interaction.response.edit_message(content=f"Saved {self._key}.", view=SettingsView(self._config))
--- cogs/gpt/classes.py
+++ cogs/gpt/classes.py
@@ -2,24 +2,16 @@ from util.config import Config
 
 
 class GPTConfig(Config):
-    model: str
     chat_model: str
-    code_model: str
     system_message: str
     temperature: float
-    code_temperature: float
-    max_tokens: int
     presence_penalty: float
 
     def __init__(self):
         super().__init__("ai")
 
     def _init_defaults(self):
-        self.model = "text-davinci-003"
         self.chat_model = "gpt-3.5-turbo"
-        self.code_model = "code-davinci-002"
         self.system_message = "You are a helpful assistant."
         self.temperature = 0.1
-        self.code_temperature = 0.1
-        self.max_tokens = 512
         self.presence_penalty = 0.5
