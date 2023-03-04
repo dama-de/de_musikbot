5323188f23e594f9e5eae6805167b57fc0694382
Dama <dam4rusxp@googlemail.com>
2023-03-04 14:57:02 +0100

    Implement conversations with the .chatgpt command

--- cogs/gpt/__init__.py
+++ cogs/gpt/__init__.py
@@ -1,21 +1,26 @@
+import logging
 import re
+from typing import List
 
 import discord
-from discord import Interaction
+from discord import Interaction, Message
 from discord.ext.commands import Cog, Bot, Context, hybrid_command, is_owner
 
 import util
 from cogs.gpt.ai import AIError
 from cogs.gpt.classes import GPTConfig
 
+_log = logging.getLogger(__name__)
+
 
 async def setup(bot: Bot):
-    await bot.add_cog(GPT())
+    await bot.add_cog(GPT(bot))
 
 
 class GPT(Cog):
-    def __init__(self) -> None:
+    def __init__(self, bot: Bot) -> None:
         self._config = GPTConfig()
+        self._bot = bot
 
     @hybrid_command(hidden=True)
     @is_owner()
@@ -67,6 +72,65 @@ class GPT(Cog):
                 await util.split_message(text, ctx)
             except AIError as e:
                 await ctx.reply(str(e))
+
+    @Cog.listener()
+    async def on_message(self, message: Message):
+        """Allow users to reply to a ChatGPT command to continue the conversation.
+        This method filters for valid messages, retrieves the conversation history and creates a reply."""
+
+        # If this is not a reply or the replied to message cannot be found, ignore
+        if not message.reference or not isinstance(message.reference.resolved, Message):
+            return
+
+        # If the replied to message is not ours, ignore
+        if message.reference.resolved.author.id is not self._bot.user.id:
+            return
+
+        # If the replied to message is not part of a GPT conversation, ignore
+        # Walk up the conversation and see if it starts with a GPT command or interaction
+        history = await self.get_reply_history(message)
+        initial = history[0]
+
+        # Check that the reply history originates in a valid conversational gpt command
+        valid_cmds = ["chatgpt"]
+        initial_ctx = await self._bot.get_context(initial)
+        if initial.author.id is self._bot.user.id and initial.interaction and initial.interaction.name in valid_cmds:
+            pass
+        elif initial_ctx.valid and initial_ctx.command.name in valid_cmds:
+            pass
+        else:
+            return
+
+        api_history = [
+            {
+                "role": "assistant" if msg.author.bot else "user",
+                "content": msg.clean_content
+            }
+            for msg in history
+        ]
+        api_history.insert(0, {"role": "system", "content": self._config.system_message})
+
+        ctx = await self._bot.get_context(message)
+        async with ctx.typing():
+            try:
+                reply = await ai.history_completion(api_history, model=self._config.chat_model,
+                                                    temperature=self._config.code_temperature,
+                                                    presence_penalty=self._config.presence_penalty,
+                                                    user=message.author.name)
+
+                await util.split_message(reply, ctx)
+            except AIError as e:
+                await ctx.reply(str(e))
+
+    async def get_reply_history(self, message: Message) -> List[Message]:
+        history = [message]
+        while message.reference:
+            if not message.reference.resolved:
+                message.reference.resolved = await message.channel.fetch_message(message.reference.message_id)
+
+            history.insert(0, message.reference.resolved)
+            message = message.reference.resolved
+        return history
 
 
 class SettingsView(discord.ui.View):
--- cogs/gpt/ai.py
+++ cogs/gpt/ai.py
@@ -11,21 +11,25 @@ async def completion(prompt, *, model: str = "text-davinci-003", **kwargs):
         raise AIError(str(e))
 
 
+async def history_completion(history, *, model: str = "gpt-3.5-turbo", **kwargs):
+    try:
+        completion = openai.ChatCompletion.create(
+            model=model,
+            messages=history,
+            **kwargs
+        )
+        return completion.choices[0].message.content
+    except openai.error.OpenAIError as e:
+        raise AIError(str(e))
+
+
 async def chat_completion(prompt, system_message, *, model: str = "gpt-3.5-turbo", **kwargs):
     messages = [
         {"role": "system", "content": system_message},
         {"role": "user", "content": prompt},
     ]
 
-    try:
-        completion = openai.ChatCompletion.create(
-            model=model,
-            messages=messages,
-            **kwargs
-        )
-        return completion.choices[0].message.content
-    except openai.error.OpenAIError as e:
-        raise AIError(str(e))
+    return await history_completion(messages, model=model, **kwargs)
 
 
 class AIError(Exception):
